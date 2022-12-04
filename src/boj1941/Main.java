c716792956429b8b7451b0a3393cf4cf290d9fc3
suyeon-jung <wjdjsy210@gmail.com>
2022-12-04 15:28:39 +0900

    :sparkles: 백준/1941/소문난 칠공주

---
+++ src/boj1941/Main.java
@@ -0,0 +1,121 @@
+package boj1941;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class Main {
+
+    static int N = 5;
+    static int SEVEN = 7;
+
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    static char[][] board = new char[N][N];
+    static Set<Integer> selected = new HashSet<>(); // 선택 여부 판단 배열
+    static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        // 1. 입력부
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+        /**
+         * 1. 7명의 여학생(7명의 자리 위치 어떻게 뽑아내지??) -> 조합으로 자리 뽑아내기
+         * 2. 가로나 세로로 반드시 인접 -> 인접한지 확인
+         * 3. 이다솜파(S) 학생들로만 구성될 필요는 없지만 이다솜파(S) > 임도연파(Y) => 이다솜파(S)가 적어도 4명 이상 포함
+         */
+        getCombinations(0, 0, 0);
+
+        System.out.println(ans);
+
+    }
+
+    static void getCombinations(int index, int depth, int count) {
+
+        if (depth == SEVEN) {
+            if(bfs()) {
+                ans++;
+            }
+            return;
+        }
+
+        // 탈출 조건
+        if (index >= N * N) {
+            return;
+        }
+
+        // 0 ~ 24 에서 7개 뽑기
+        getCombinations(index + 1, depth, count); // 안뽑는경우
+        selected.add(index);
+        if (board[index / 5][index % 5] == 'S') { // 이다솜
+            getCombinations(index + 1, depth + 1, count);
+        } else {
+            if(count < 3) {
+                getCombinations(index + 1, depth + 1, count + 1);
+            }
+        }
+        selected.remove(index);
+
+
+    }
+
+    // 넓이가 7이어야 만족, 인접한지 판단
+    static boolean bfs() {
+        int start = 0;
+        for (int i = 0; i < N * N; i++) {
+            if (selected.contains(i)) {
+                start = i;
+                break;
+            }
+        }
+
+        int area = 0;
+
+        boolean[] visited = new boolean[N*N];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            int curRow = cur / 5;
+            int curCol = cur % 5;
+            area++;
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nextRow = curRow + dx[dir];
+                int nextCol = curCol + dy[dir];
+                int next = nextRow * N + nextCol;
+
+                if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N
+                    || visited[next] || !selected.contains(next)) {
+                    continue;
+                }
+
+                // 후보군에 있어야 다음 탐색 노드로 설정 가능
+                q.offer(next);
+                visited[next] = true;
+
+            }
+
+        }
+
+        if (area == 7) { // 이경우에만 모든 노드가 인접한 것임!
+            return true;
+        }
+        return false;
+    }
+
+}
