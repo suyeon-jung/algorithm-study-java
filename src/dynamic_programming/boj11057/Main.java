cd1013b81389cdb9b56fed229c0ede682918529a
suyeon-jung <wjdjsy210@gmail.com>
2022-12-05 22:38:33 +0900

    :recycle: 백준/11057/오르막 수

--- src/dynamic_programming/boj11057/Main.java
+++ src/dynamic_programming/boj11057/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
     static int[][] dp; // dp[i][j] = i 자리수일때 뒷자리가 j인 경우 오르막수 개수
 
+    static int MOD = 10_007;
+
     public static void main(String[] args) throws IOException {
         // 입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +25,7 @@ public class Main {
             for (int j = 0; j < 10; j++) {
                 for (int k = 0; k <= j; k++) { // 마지막 자리수가 j인 경우 그 앞에 올 수 있는 숫자는 0~j
                     dp[i][j] += dp[i - 1][k];
-                    dp[i][j] %= 10007;
+                    dp[i][j] %= MOD;
                 }
             }
         }
@@ -33,6 +35,6 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             ans += dp[length][i];
         }
-        System.out.println(ans % 10007);
+        System.out.println(ans % MOD);
     }
 }
