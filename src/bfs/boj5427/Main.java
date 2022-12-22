9081f31cd76ccb0c8b2bcf9eb2ad9bfe21384760
suyeon-jung <wjdjsy210@gmail.com>
2022-12-22 19:58:34 +0900

    :recycle: 백준/5427/불

--- src/bfs/boj5427/Main.java
+++ src/bfs/boj5427/Main.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -16,65 +17,66 @@ public class Main {
     static Queue<int[]> fire;
     static Queue<int[]> sanguen;
     static int[][] visited;
-
     static int[][] visitedSanguen;
 
     public static void main(String[] args) throws IOException {
-        int testcaseCount, width, height;
-
         // 입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        testcaseCount = Integer.parseInt(s);
-
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        String line;
+        int tcNum, w, h;
+        char[][] board;
 
-        for(int t = 0; t < testcaseCount; t++) {
+        tcNum = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t < tcNum; t++) {
             sanguen = new LinkedList<>();
             fire = new LinkedList<>();
 
-            String[] str = br.readLine().split(" ");
-            width = Integer.parseInt(str[0]);
-            height = Integer.parseInt(str[1]);
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
 
-            visited = new int[height][width];
-            visitedSanguen = new int[height][width];
+            board = new char[h][w];
+            visited = new int[h][w];
+            visitedSanguen = new int[h][w];
 
-            for(int i = 0; i < height; i++) {
+            for(int i = 0; i < h; i++) {
                 Arrays.fill(visited[i], Integer.MAX_VALUE);
                 Arrays.fill(visitedSanguen[i], -1);
             }
 
-
-            char[][] board = new char[height][width];
-
-            for (int i = 0; i < height; i++) {
-                String tmp = br.readLine();
-                for (int j = 0; j < width; j++) {
-                    board[i][j] = tmp.charAt(j);
-                    if (tmp.charAt(j) == '*') { // 불
+            for (int i = 0; i < h; i++) {
+                line = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    board[i][j] = line.charAt(j);
+                    if (line.charAt(j) == '*') { // 불
                         fire.offer(new int[]{i, j});
                         visited[i][j] = 0;
-                    } else if(tmp.charAt(j) == '@') {
+                    } else if(line.charAt(j) == '@') { // 상근
                         sanguen.offer(new int[]{i, j});
                         visitedSanguen[i][j] = 0;
                     }
                 }
             }
 
-
             // 불에 대한 bfs 수행
             while (!fire.isEmpty()) {
                 int[] cur = fire.poll();
+                int cx = cur[0];
+                int cy = cur[1];
+
                 for (int dir = 0; dir < 4; dir++) {
-                    int nx = cur[0] + dx[dir];
-                    int ny = cur[1] + dy[dir];
+                    int nx = cx + dx[dir];
+                    int ny = cy + dy[dir];
+
                     // 범위 벗어남, 이미 방문함, 벽
-                    if (nx < 0 || nx >= height || ny < 0 || ny >= width || visited[nx][ny] != Integer.MAX_VALUE || board[nx][ny] == '#' ) {
+                    if (isOOB(w, h, nx, ny) || visited[nx][ny] != Integer.MAX_VALUE || board[nx][ny] == '#' ) {
                         continue;
                     }
 
-                    visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
+                    visited[nx][ny] = visited[cx][cy] + 1;
                     fire.offer(new int[] {nx, ny});
                 }
             }
@@ -83,11 +85,10 @@ public class Main {
 
             while(!sanguen.isEmpty()) {
                 int[] cur = sanguen.poll();
-
                 int cx = cur[0];
                 int cy = cur[1];
 
-                if(cx == 0 || cx == height - 1 || cy == 0 || cy == width - 1) {
+                if(cx == 0 || cx == h - 1 || cy == 0 || cy == w - 1) {
                     answer = visitedSanguen[cx][cy] + 1;
                     break;
                 }
@@ -97,21 +98,23 @@ public class Main {
                     int ny = cy + dy[dir];
                     int nextTime =  visitedSanguen[cx][cy] + 1;
 
-                    if(nx < 0 || nx >= height || ny < 0 || ny >= width || board[nx][ny] == '#' || visited[nx][ny] <= nextTime || visitedSanguen[nx][ny] != -1) {
+                    if(isOOB(w, h, nx, ny) || board[nx][ny] == '#' || visited[nx][ny] <= nextTime || visitedSanguen[nx][ny] != -1) {
                         continue;
                     }
+
                     visitedSanguen[nx][ny] = nextTime;
                     sanguen.offer(new int[] {nx, ny});
                 }
             }
 
-            if(answer == -1) {
-                sb.append("IMPOSSIBLE").append(lineSeparator());
-            } else {
-                sb.append(answer).append(lineSeparator());
-            }
+            sb.append(answer == -1 ?  "IMPOSSIBLE" : answer).append(lineSeparator());
         }
 
         System.out.println(sb);
+        br.close();
+    }
+
+    private static boolean isOOB(int w, int h, int x, int y) {
+        return x < 0 || x >= h || y < 0 || y >= w;
     }
 }
